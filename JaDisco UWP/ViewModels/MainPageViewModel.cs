a0862fff41057f1c1c44e11788fa93ab621e575f
Kacper Tryniecki <kacper@tryniecki.com>
2019-02-16 14:39:33 +0100

    Wypolerowane parsowanie statusu, dodana opcja na zmiane streamu Wonziu lub Dzej

--- JaDisco UWP/ViewModels/MainPageViewModel.cs
+++ JaDisco UWP/ViewModels/MainPageViewModel.cs
@@ -1,14 +1,14 @@
-﻿using HtmlAgilityPack;
-using System;
+﻿using System;
+using Windows.ApplicationModel.Core;
 using Windows.System;
+using Windows.UI;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml.Controls;
 
 namespace JaDisco_UWP.ViewModels
 {
     internal class MainPageViewModel
     {
-        public string Status { get; set; }
-
         public async void MemoryCleanup()
         {
             await WebView.ClearTemporaryWebDataAsync();
@@ -18,6 +18,21 @@ namespace JaDisco_UWP.ViewModels
         public async void LaunchUri(Uri uri)
         {
             _ = await Launcher.LaunchUriAsync(uri);
-        } 
+        }
+
+        public void TitleBarCustomization()
+        {
+            CoreApplicationViewTitleBar CoreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            CoreTitleBar.ExtendViewIntoTitleBar = false;
+
+            ApplicationViewTitleBar titleBar = ApplicationView.GetForCurrentView().TitleBar;
+            titleBar.ForegroundColor = Colors.White;
+            titleBar.BackgroundColor = Colors.Black;
+            titleBar.ButtonForegroundColor = Colors.White;
+            titleBar.ButtonBackgroundColor = Colors.Black;
+
+            titleBar.InactiveBackgroundColor = Colors.Black;
+            titleBar.ButtonInactiveBackgroundColor = Colors.Black;
+        }
     }
 }
--- JaDisco UWP/Views/MainPage.xaml.cs
+++ JaDisco UWP/Views/MainPage.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using HtmlAgilityPack;
 using JaDisco_UWP.ViewModels;
 using System;
-using Windows.ApplicationModel.Core;
-using Windows.UI;
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -15,7 +13,8 @@ namespace JaDisco_UWP
     {
         private bool LeftChat, HiddenChat = false;
         private readonly Uri ChatUri = new Uri("https://client.poorchat.net/jadisco");
-        private readonly Uri StreamUri = new Uri("https://player.twitch.tv/?channel=wonziu");
+        private readonly Uri WonziuUri = new Uri("https://player.twitch.tv/?channel=wonziu");
+        private readonly Uri DzejUri = new Uri("https://player.twitch.tv/?channel=dzejth");
         private readonly ToolTip chatHideToolTip = new ToolTip();
 
         private readonly ApplicationView view = ApplicationView.GetForCurrentView();
@@ -27,18 +26,7 @@ namespace JaDisco_UWP
         {
             InitializeComponent();
             StartStatusParse();
-
-            CoreApplicationViewTitleBar CoreTitleBar = CoreApplication.GetCurrentView().TitleBar;
-            CoreTitleBar.ExtendViewIntoTitleBar = false;
-
-            ApplicationViewTitleBar titleBar = ApplicationView.GetForCurrentView().TitleBar;
-            titleBar.ForegroundColor = Colors.White;
-            titleBar.BackgroundColor = Colors.Black;
-            titleBar.ButtonForegroundColor = Colors.White;
-            titleBar.ButtonBackgroundColor = Colors.Black;
-
-            titleBar.InactiveBackgroundColor = Colors.Black;
-            titleBar.ButtonInactiveBackgroundColor = Colors.Black;
+            vm.TitleBarCustomization();
 
             webView.NavigationCompleted += webView_NavigationCompleted;
             webView.NavigationStarting += webView_NavigationStarting;
@@ -163,7 +151,7 @@ namespace JaDisco_UWP
 
         private void RefreshButton_Click(object sender, RoutedEventArgs e)
         {
-            StreamWebView.Source = StreamUri;
+            //StreamWebView.Source = StreamUri;
             StreamWebView.Refresh();
 
             ChatWebView.Source = ChatUri;
@@ -172,10 +160,14 @@ namespace JaDisco_UWP
             StartStatusParse();
         }
 
+        private void SwitchButton_Click(object sender, RoutedEventArgs e)
+        {
+            FlyoutBase.ShowAttachedFlyout((FrameworkElement)sender);
+        }
+
         private void SettingsButton_Click(object sender, RoutedEventArgs e)
         {
             FlyoutBase.ShowAttachedFlyout((FrameworkElement)sender);
-            vm.MemoryCleanup();
         }
 
         private void DonateButton_Click(object sender, RoutedEventArgs e)
@@ -203,22 +195,42 @@ namespace JaDisco_UWP
             if (args.IsSuccess == true)
             {
                 string HTML = await webView.InvokeScriptAsync("eval", new string[] { "document.documentElement.outerHTML;" });
-
                 HtmlDocument htmlDoc = new HtmlDocument();
-
                 htmlDoc.LoadHtml(HTML);
 
                 //flex-navbar-item jd-title
-                string Status = htmlDoc.DocumentNode.SelectSingleNode("//div[@class='flex-navbar-item jd-title']").InnerText;
+                StatusTextBlock.Text = htmlDoc.DocumentNode.SelectSingleNode("//div[@class='flex-navbar-item jd-title']").InnerText;
 
-                StatusTextBlock.Text = Status;
                 ProgressBar.Visibility = Visibility.Collapsed;
                 vm.MemoryCleanup();
             }
         }
 
+        //private void WonziuButton_Checked(object sender, RoutedEventArgs e)
+        //{
+        //    StreamWebView.Navigate(WonziuUri);
+        //}
+
+        private void DzejButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (IsDzej.IsChecked == true)
+            {
+                StreamWebView.Navigate(DzejUri);
+            }
+            else if (IsDzej.IsChecked == false)
+            {
+                StreamWebView.Navigate(WonziuUri);
+            }
+        }
+
+        private void GarbageButton_Click(object sender, RoutedEventArgs e)
+        {
+            vm.MemoryCleanup();
+        }
+
         private void webView_NavigationStarting(WebView sender, WebViewNavigationStartingEventArgs args)
         {
+            StatusTextBlock.Text = "Ładowanie statusu...";
             ProgressBar.Visibility = Visibility.Visible;
         }
     }
