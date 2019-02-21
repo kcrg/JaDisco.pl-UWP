56da99f77337b0bbfd59d1d5f5ef902baaa25d65
Kacper Tryniecki <kacper@tryniecki.com>
2019-02-21 02:27:42 +0100

    Added custom message dialog

---
+++ JaDisco UWP/Views/CustomDialogs/ErrorDialog.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace JaDisco_UWP.Views.CustomDialogs
+{
+    public sealed partial class ErrorDialog : ContentDialog
+    {
+        public ErrorDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+            Hide();
+        }
+    }
+}
